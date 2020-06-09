ed6c35dcb4450d50f08022aa03c3226a864e0fae
Damian Galetto <damian.galetto@gmail.com>
2020-06-09 16:35:36 -0300

    Test modules

--- index.d.ts
+++ index.d.ts
@@ -6,29 +6,5 @@
  */
 export as namespace yappa;
 
-/*~ You can declare types that are available via importing the module */
-export interface someType {
-    name: string;
-    length: number;
-    extras?: string[];
-}
 
-/*~ You can declare properties of the module using const, let, or var */
-export enum enumType {
-    PEPE = 1,
-    MARIA = 2
-}
-
-/*~ If there are types, properties, or methods inside dotted names
- *~ of the module, declare them inside a 'namespace'.
- */
-export namespace subProp {
-    /*~ For example, given this definition, someone could write:
-     *~   import { subProp } from 'yourModule';
-     *~   subProp.foo();
-     *~ or
-     *~   import * as yourMod from 'yourModule';
-     *~   yourMod.subProp.foo();
-     */
-    function foo(): void;
-}
+export * as ReadStorage from "read-storage";
---
+++ read-storage/index.d.ts
@@ -0,0 +1,8 @@
+export namespace ReadStorage {
+    export interface CommentListUser {
+        name: string;
+        profilePic: string;
+        authId: number;
+        verified: number;
+    }
+}
